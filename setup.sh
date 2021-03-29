3aea9fd0601451a68128911df1e003dc94816403
Zeeshan Khan <zkhan1093@gmail.com>
2021-03-29 13:35:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #/bin/bash
 REPO_DIR=$PWD
 
+sudo apt-get install gcc python3-dev python3-setuptools python3-pip
+python3 -m pip install virtualenv
+
 echo Creating virtual environment from python3
-virtualenv -p python3 $PWD/venv
+/home/${USER}/.local/bin/virtualenv -p python3 $PWD/venv
 
 $REPO_DIR/venv/bin/pip install -r $REPO_DIR/requirements.txt
 sed -i "s+REPO_DIR+${REPO_DIR}+g" $REPO_DIR/power_button.service
